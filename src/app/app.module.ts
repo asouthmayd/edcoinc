8fd9ce245dc77c2d49fdd99eca1f05f1571ae77c
Anthony <as0317@mix.wvu.edu>
2022-01-23 15:02:00 -0500

    added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { mainNavBarComponent } from 'src/app/navigation/mainNavBar.component';
+import { mainNavBarComponent } from 'src/navigation/mainNavBar.component';
 
 import { AppComponent } from './app.component';
 
